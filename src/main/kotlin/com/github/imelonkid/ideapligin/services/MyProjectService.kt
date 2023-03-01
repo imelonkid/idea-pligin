5581ce693397d91276e17353fa01bfc6a63c9839
GitHub Action <action@github.com>
2023-03-01 12:22:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/imelonkid/ideapligin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/imelonkid/ideapligin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.imelonkid.ideapligin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.imelonkid.ideapligin.MyBundle
 
 class MyProjectService(project: Project) {
 
